7437e35c33b0ade1c525a5d6ed33d05c748ee582
Rishabh Jain <jrishabh424@gmail.com>
2019-08-06 12:43:51 +0530

    Update setup_Longclaw.py

--- setup_Longclaw.py
+++ setup_Longclaw.py
@@ -4,5 +4,5 @@ from cx_Freeze import setup, Executable
 setup(
     name = "Longclaw",
     version = "1.0",
-    description = "Longclaw is a Photo enhance and photo editing tool developed by Rishabh Jain and Rishabh Kumar as Mini Project",
+    description = "Longclaw is a Photo enhance and photo editing tool developed by Rishabh Jain and Rishabh Kumar.",
     executables = [Executable("yourFileName.py", base = "Win32GUI")])
